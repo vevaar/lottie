b7b355648095cc2e41b79f3f75af8b1fd7ba2edc
vevaar <vevaaratvav@gmail.com>
2024-05-21 11:40:52 +0530

    file input option added

--- src/Anime.jsx
+++ src/Anime.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Lottie from "lottie-react";
-import groovyWalkAnimation from "./groovyWalk.json";
 
 const Anime = () => {
-  return (
-    <Lottie animationData={groovyWalkAnimation} />
-  )
-}
+  const [animationData, setAnimationData] = useState(null);
 
-export default Anime
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type === "application/json") {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const json = JSON.parse(e.target.result);
+        setAnimationData(json);
+      };
+      reader.readAsText(file);
+    } else {
+      alert("Please upload a valid JSON file.");
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept="application/json" onChange={handleFileUpload} />
+      {animationData && <Lottie animationData={animationData} />}
+    </div>
+  );
+};
+
+export default Anime;
