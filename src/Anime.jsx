2e7eda85c355907684b9f60ccf5e436272f37f35
vevaar <vevaaratvav@gmail.com>
2024-05-21 11:32:51 +0530

    first commit

---
+++ src/Anime.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Lottie from "lottie-react";
+import groovyWalkAnimation from "./groovyWalk.json";
+
+const Anime = () => {
+  return (
+    <Lottie animationData={groovyWalkAnimation} />
+  )
+}
+
+export default Anime
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Lottie from "lottie-react";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import groovyWalkAnimation from "./groovyWalk.json";
+import Anime from './Anime';
+
+
+function App() {
+
+  return(
+    <Anime />
+  )}
+
+export default App
